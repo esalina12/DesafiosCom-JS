ccdfea9fc6c6abd337cc99163fd794f6a0ed9066
Ezequiel Salina <eze2334@gmail.com>
2022-09-04 23:15:59 -0300

    Creando nueva rama desafio 2

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,15 +1,9 @@
-debugger
 
-/* let num = parseInt(prompt('Ingresa un número:'))
-for (let i=0; i<=num; i++){
-    if ((i / 5) == 1){
-        continue
-    }
-    console.log(i)
-} */
+// ------------------------------ Desafío Complementario 1 -------------------
 
-let club = prompt('Indica un equipo de fùtbol')
+/* let club = prompt('Indica un equipo de fùtbol')
 club.toLowerCase()
+debugger
 while (club!='esc'){
     switch(club){
         case 'boca juniors':
@@ -31,8 +25,50 @@ while (club!='esc'){
             alert('Lo siento. Debe ser uno de los 5 equipos más populares de Argentina. (Escriba "esc" sin las comillas para finalizar)')
     }
     club = prompt('Ingresa otro equipo')
-}
-/* if (club===true){
-    alert('Su equipo es '+club)
 } */
+// ---------------- Fin desafío complementario 1 -------------------
 
+
+
+//---------------- Desafío complementario 2 --------------------------
+const consulta = 1000
+let precio = 0
+class Paciente {
+    constructor(nombre, apellido, dni, condicion) {
+        this.nombre = nombre,
+        this.apellido = apellido,
+        this.dni = parseInt(dni)
+        this.condicion = condicion.toUpperCase()
+    }
+    costo(){
+        if (this.condicion==='PARTICULAR'){
+            precio = 2000 + consulta
+        } else if (this.condicion==='OSDE'){
+            precio = 1000 + consulta
+        } else { alert('Particular u osde')
+            }
+    }
+    mostrar(){
+        if((this.condicion==='PARTICULAR') || (this.condicion==='OSDE')){
+           alert('Los datos del paciente son: ' + this.nombre +' '+ this.apellido +' '+ this.dni +' '+ this.condicion+' '+'Su costo es: '+precio) 
+        }
+    }
+}
+
+const pacientes=[]
+
+function costoPaciente (){
+    debugger
+    let nombre = prompt('Ingrese su nombre:')
+    let apellido = prompt('Ingrese su apellido:')
+    let dni = parseInt(prompt('Ingrese su dni:'))
+    let condicion = prompt('Ingrese si es particular o indique su obra social:')
+    const pacie = {nombre: nombre, apellido: apellido, dni: dni, condicion: condicion}
+    pacientes.push(pacie)
+    const costo = new Paciente(nombre,apellido,dni,condicion)
+    costo.costo()
+    costo.mostrar()
+    console.table(pacientes)
+}
+
+//---------------- Fin desafío complementario 2-----------------------
