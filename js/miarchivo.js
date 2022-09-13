56888972aa6ef94d0be315c119cbb4dfbf06a55f
Ezequiel Salina <eze2334@gmail.com>
2022-09-12 21:40:46 -0300

    Desafio complementario 3

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -31,7 +31,7 @@ while (club!='esc'){
 
 
 //---------------- Desafío complementario 2 --------------------------
-const consulta = 1000
+/* const consulta = 1000
 let precio = 0
 class Paciente {
     constructor(nombre, apellido, dni, condicion) {
@@ -69,6 +69,88 @@ function costoPaciente (){
     costo.costo()
     costo.mostrar()
     console.table(pacientes)
+} */
+
+//---------------- Fin desafío complementario 2--------------------------------------------
+
+
+
+
+
+//--------------------------------- Desafio complementario 3--------------------------------
+
+
+
+const especialistas = [
+    {nombre: 'Mario Bross', espe: 'OFTALMOLOGIA'},
+    {nombre: 'Esteban Quito', espe: 'MEDICO CLINICO'},
+    {nombre: 'Adriana Gonzales', espe: 'GINECOLOGIA'},
+    {nombre: 'Armando Paredes', espe: 'NUTRICION'}
+]
+
+
+const consulta = 1000
+let precio = 0
+
+class Paciente {
+    constructor(nombre, apellido, dni, condicion) {
+        this.nombre = nombre,
+        this.apellido = apellido,
+        this.dni = parseInt(dni)
+        this.condicion = condicion.toUpperCase()
+    }
+
+    costo(){
+        if (this.condicion==='PARTICULAR'){
+            precio = 2000 + consulta
+        } else if (this.condicion==='OSDE'){
+            precio = 1000 + consulta
+        } else { alert('Particular u osde')
+            }
+    }
+
+    mostrar(){
+        if((this.condicion==='PARTICULAR') || (this.condicion==='OSDE')){
+           alert('Los datos del paciente son: ' + this.nombre +' '+ this.apellido +' '+ this.dni +' '+ this.condicion+'.'+'Su costo es: '+precio+'')
+        }
+    }
 }
 
-//---------------- Fin desafío complementario 2-----------------------
+const pacientes=[]
+function pac (pacientes){
+    console.log (pacientes)
+}
+
+
+function presupuestoPaciente (){
+    //debugger
+    let nombre = (prompt('Ingrese su nombre:')).toUpperCase()
+    let apellido = (prompt('Ingrese su apellido:')).toUpperCase()
+    let dni = parseInt(prompt('Ingrese su dni:'))
+    let condicion = (prompt('Ingrese si es particular o afiliado OSDE:')).toUpperCase()
+    let especial = (prompt('Elija una especialidad: Oftalmología, Medico clinico, Ginecología o Nutrición')).toUpperCase()
+
+
+    const pacie = {nombre: nombre, apellido: apellido, dni: dni, condicion: condicion}
+    pacientes.push(pacientes)
+
+    const espec = especialistas.filter(especialista => especialista.espe.includes(especial))
+
+    const costo = new Paciente(nombre,apellido,dni,condicion)
+    costo.costo()
+    
+    let datosPaciente = document.createElement('div')
+
+    datosPaciente.innerHTML = `<p> Nombre: ${nombre}</p>
+                                <p> Apellido: ${apellido}</p>
+                                <p> DNI: ${dni}</p>
+                                <p> Condicion: ${condicion}</p>
+                                <p> Especialidad: ${especial}</p>`
+    
+    document.body.appendChild(datosPaciente)
+}
+
+presupuestoPaciente()
+
+//-----------------------------Fin Desafío 3 -------------------------------------------
+
